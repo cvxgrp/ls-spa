cc0d6c23c8482db1c8a61598fcd704f47dce25f6
Nikhil Devanathan <nikhil.devanathan@gmail.com>
2023-10-31 16:46:22 -0700

    cleanup and qol

--- setup.py
+++ setup.py
@@ -1,15 +1,35 @@
-from setuptools import setup
+#!/usr/bin/python
+# Credit to Parth Nobel for this file
+import subprocess
+from setuptools import setup, find_packages
 
-setup(name='funniest',
-      version='0.1',
-      description='Efficient Shapley performance attribution for least-squares problems',
-      url='https://github.com/cvxgrp/ls-spa',
-      license='MIT',
-      packages=['ls_spa'],
-      install_requires=[
+# get all the git tags from the cmd line that follow our versioning pattern
+git_tags = subprocess.Popen(
+    ["git", "tag", "--list", "v*[0-9]", "--sort=version:refname"],
+    stdout=subprocess.PIPE,
+)
+tags = git_tags.stdout.read()
+git_tags.stdout.close()
+tags = tags.decode("utf-8").split("\n")
+tags.sort()
+
+# PEP 440 won't accept the v in front, so here we remove it, strip the new line and decode the byte stream
+VERSION_FROM_GIT_TAG = tags[-1][1:]
+
+setup(
+    name="ls_spa",
+    version=VERSION_FROM_GIT_TAG,  # Required
+    setup_requires=["setuptools>=18.0"],
+    packages=find_packages(exclude=["notebooks"]),  # Required
+    install_requires=[
           'numpy',
           'scipy',
           'pandas',
           'matplotlib',
       ],
-      zip_safe=False)
+    description="Efficient Shapley performance attribution for least-squares problems",
+    url="https://github.com/cvxgrp/ls-spa",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
